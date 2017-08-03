8dd2d95ce0649b932780b7622027e3e2025ccf52
sebtn <sdebedout@gmail.com>
2017-08-03 16:17:18 -0400

    Proptypes and other bindAcionCreators patterns

--- app/components/Courses.js
+++ app/components/Courses.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 
 import Header from './Header'
 import {createCourse} from '../actions/index'
@@ -74,7 +75,7 @@ let mapStateToProps = (state) => {
 /*--------------------------------------------------*/
 let mapDispatchToProps = (dispatch) => {
   return {
-    createCourse: course => dispatch(createCourse(course))
+    createCourse: bindActionCreators(createCourse, dispatch)
   }
 }
 
