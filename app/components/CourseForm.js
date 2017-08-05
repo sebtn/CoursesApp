2fda144454be618e8ec36c28f28bf5b59407b983
sebtn <sdebedout@gmail.com>
2017-08-05 10:41:17 -0400

    Css added  to form

--- app/components/CourseForm.js
+++ app/components/CourseForm.js
@@ -10,41 +10,41 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) =>{
       <form>
       <h1>Manage Course</h1>
         <TextInput 
-        name="title"
-        label="Title"
-        value={course.title}
-        onChange={onChange}
-        error={errors.title} />
+          name="title"
+          label="Title"
+          value={course.title}
+          onChange={onChange}
+          error={errors.title} />
 
         <SelectInput 
-        name="authorId"
-        label="Author"
-        value={course.authorId}
-        defualtOptions="Select Author"
-        options={allAuthors}
-        onChange={onChange}
-        error={errors.authorId} />
+          name="authorId"
+          label="Author"
+          value={course.authorId}
+          defualtOptions="Select Author"
+          options={allAuthors}
+          onChange={onChange}
+          error={errors.authorId} />
 
         <TextInput 
-        name="category"
-        label="Category"
-        value={course.category}
-        onChange={onChange}
-        error={errors.category} />
+          name="category"
+          label="Category"
+          value={course.category}
+          onChange={onChange}
+          error={errors.category} />
 
         <TextInput 
-        name="length"
-        label="Length"
-        value={course.length}
-        onChange={onChange}
-        error={errors.length} />
+          name="length"
+          label="Length"
+          value={course.length}
+          onChange={onChange}
+          error={errors.length} />
 
         <input 
-        type="submit"
-        disabled={loading}
-        value={loading ? "Saving..." : "Save"}
-        className="btn btn-primary"
-        onClick={onSave} />
+          type="submit"
+          disabled={loading}
+          value={loading ? "Saving..." : "Save"}
+          className="btn btn-primary"
+          onClick={onSave} />
       </form>
     </div>
   )
